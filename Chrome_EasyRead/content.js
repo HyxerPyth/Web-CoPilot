65dca7f5aa65c5dccae0367bef9901cd8fab7d43
Roman Loboda <hyawey2003@gmail.com>
2023-12-28 21:29:47 -0500

    new commit
    commit

--- Chrome_EasyRead/content.js
+++ Chrome_EasyRead/content.js
@@ -1,17 +1,14 @@
 // content.js
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === 'copyPageContent') {
-    // Get the text content before removing elements
-    var bodyText = document.body.innerText;
-    
-    // Remove all elements from the document
-    document.body.innerHTML = '';
-    document.body.innerHTML = bodyText;
+    // Get the outer HTML of the body element
+    var bodyHtml = document.body.innerText;
+
+    gptRequest(bodyHtml);
 
     // Store the copied content
-    chrome.storage.local.set({ 'copiedContent': bodyText }, function () {
-      console.log('Copied content stored:', bodyText);
-      clipBoard = bodyText;
+    chrome.storage.local.set({ 'copiedContent': bodyHtml }, function () {
+      console.log('Copied content stored:', bodyHtml);
     });
 
   } else if (request.action === 'pasteCopiedContent') {
@@ -22,10 +19,51 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   }
 });
 
-let clipBoard 
-
-
 function insertCopiedContent(content) {
-  // Insert the copied content into the document (replace the body)
-  document.body.innerHTML = '<pre>' + content + '</pre>';
+  // Create a temporary div element
+  var tempDiv = document.createElement('div');
+  
+  // Set the innerHTML of the div to the copied content
+  tempDiv.innerHTML = content;
+
+  // Replace the body with the content of the div
+  document.body.parentNode.replaceChild(tempDiv.firstChild, document.body);
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === 'pasteCopiedContent') {
+    chrome.storage.local.get('copiedContent', function (data) {
+      var copiedContent = data.copiedContent || 'No content copied yet.';
+      insertCopiedContent(copiedContent);
+    });
+  }
+});
+
+
+// GPT REQUEST 
+
+async function gptRequest(textHTML) {
+  try {
+    const response = await fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer 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"
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        prompt: `Give one paragraph summary of what is the difference between arrow fucntion and regualr function: ${textHTML}`,
+        max_tokens: 2000
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    console.log(responseData.choices[0].text)
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 }
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      ChatGPT
+      <textarea></textarea>
+    </div>
+  );
+}
+
+export default App;
